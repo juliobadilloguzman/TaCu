57b0572907be2cb79fb3f482f5656db926c210f8
Mac <mac@MacBook-Pro-de-Mac.local>
2019-10-02 16:35:56 -0500

    Proyecto Creado

---
+++ app/src/main/java/mx/tec/tacu/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package mx.tec.tacu.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
