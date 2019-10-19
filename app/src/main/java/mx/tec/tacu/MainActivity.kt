1b860a4509ac41e82a554dff00b231dfec17d528
Shannon <shanyrp@gmail.com>
2019-10-19 11:25:06 -0500

    Correccion action bar

--- app/src/main/java/mx/tec/tacu/MainActivity.kt
+++ app/src/main/java/mx/tec/tacu/MainActivity.kt
@@ -14,8 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        actionBar?.hide()
-
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
@@ -28,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
 
     }
 
