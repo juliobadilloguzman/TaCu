ac698b44347fd01b2177d730d14a2e925edbde59
Mac <mac@MacBook-Pro-de-Mac.local>
2019-10-18 23:34:47 -0500

    Firebase integrado y varios activities creados

--- app/src/main/java/mx/tec/tacu/ForgotPasswordActivity.kt
+++ app/src/main/java/mx/tec/tacu/ForgotPasswordActivity.kt
@@ -4,28 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.recover_password.*
 
-class LoginActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+
+        setContentView(R.layout.recover_password)
 
         ////////Ir a el registro
-        btnCreateAccount.setOnClickListener{
+        btnCreateAccountRecover.setOnClickListener{
 
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
 
         }
 
-        ////////Ir a olvidaste tu contraseña
-        btnForgetPassword.setOnClickListener{
-            val intent = Intent(this, ForgotPasswordActivity::class.java)
-            startActivity(intent)
-        }
-
     }
-
-
 }
---
+++ app/src/main/java/mx/tec/tacu/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package mx.tec.tacu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
