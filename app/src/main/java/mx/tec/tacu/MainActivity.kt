82304b8b37e95bb5bddf62e9a31b13c343076037
Mac <mac@MacBook-Pro-de-Mac.local>
2019-10-22 02:32:54 -0500

    Commit prueba

--- app/src/main/java/mx/tec/tacu/MainActivity.kt
+++ app/src/main/java/mx/tec/tacu/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Hola
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
