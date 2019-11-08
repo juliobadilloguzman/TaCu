4f42c76396f611ac6e0e06c8e60a3f9f1a230217
Mac <mac@MacBook-Pro-de-Mac.local>
2019-11-08 15:19:43 -0600

    Modificaciones

--- app/src/main/java/mx/tec/tacu/ui/ranking/RankingFragment.kt
+++ app/src/main/java/mx/tec/tacu/ui/ranking/RankingFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 import mx.tec.tacu.R
 
@@ -18,6 +20,21 @@ class RankingFragment : Fragment() {
     ): View? {
 
         val root = inflater.inflate(R.layout.fragment_ranking, container, false)
+
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+        val reference = db.collection("TAQUERIA").orderBy("calificacion", Query.Direction.DESCENDING).limit(3)
+
+        reference.get()
+            .addOnSuccessListener { documents ->
+
+                for(document in documents) {
+
+                    println("Posicion: " + document.data)
+
+                }
+
+            }
 
         return root
     }
